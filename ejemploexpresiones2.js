cc72121408121c5fda06dba7cafff4a8a13f03ca
diegobugagonzalez <diegobugagonzalez@gmail.com>
2022-11-15 22:22:17 -0500

    nuevos datos y ejemplos añadidos

---
+++ ejemploexpresiones2.js
@@ -0,0 +1,16 @@
+let username;
+
+let userage;
+
+let birthyear;
+
+birthyear = 2022 
+
+username = prompt("ingresa tu nombre por favor:");
+
+
+
+userage = prompt("ingresa tu edad por favor:");
+
+
+document.write(`tu nombre es ${username} tu tienes ${userage}años de edad y naciste ${birthyear}.`);
